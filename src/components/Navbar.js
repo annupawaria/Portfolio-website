f0f0f65d25823e52c3d8673b20a8909fb6341b9b
annu <annupawadia@gmail.com>
2024-07-31 16:32:28 +0530

    nbhj

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+<div class="left"></div>
+<div class="right">
+    <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li></li>
+    </ul>
+
+</div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
