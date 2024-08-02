243efcb08cff95794c45bcb6ca7b4f5f0e673e6b
annu <annupawadia@gmail.com>
2024-08-02 13:03:46 +0530

    added some new css to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,13 @@ const Navbar = () => {
   return (
     <div>
         <nav>
-<div class="left"></div>
+<div class="left">Portfolio</div>
 <div class="right">
     <ul>
         <li>Home</li>
         <li>About</li>
-        <li></li>
+        <li>Work</li>
+        <li>Contact</li>
     </ul>
 
 </div>
